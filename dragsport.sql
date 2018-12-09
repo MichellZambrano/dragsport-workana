9914721bfe5bca4821d992ee7abbbded2411a1d6
Armando <armandojamaya27@hotmail.com>
2018-12-08 23:09:12 -0430

    Adaptación de templates

--- dragsport.sql
+++ dragsport.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-12-2018 a las 14:06:21
+-- Tiempo de generación: 09-12-2018 a las 04:37:53
 -- Versión del servidor: 10.1.33-MariaDB
 -- Versión de PHP: 7.2.6
 
@@ -36,6 +36,7 @@ CREATE TABLE `users` (
   `pass` varchar(90) DEFAULT NULL,
   `birthdate` int(10) UNSIGNED DEFAULT NULL,
   `gender` enum('male','female') NOT NULL DEFAULT 'male',
+  `login_with` enum('email','fb','tw','tc') NOT NULL DEFAULT 'email',
   `social_id` varchar(150) DEFAULT NULL,
   `image` varchar(100) DEFAULT NULL,
   `tmp_pass` varchar(90) DEFAULT NULL,
@@ -47,8 +48,10 @@ CREATE TABLE `users` (
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id_user`, `first_name`, `last_name`, `email`, `pass`, `birthdate`, `gender`, `social_id`, `image`, `tmp_pass`, `token`, `created_at`) VALUES
-(1, 'test', 'test', 'test@demo.com', '$2a$10$b65a06983a40dff7b1f7euys9aDoxG9OZF40MxpT9hYq861pb/M8K', 796363200, 'male', NULL, NULL, NULL, NULL, 1544101380);
+INSERT INTO `users` (`id_user`, `first_name`, `last_name`, `email`, `pass`, `birthdate`, `gender`, `login_with`, `social_id`, `image`, `tmp_pass`, `token`, `created_at`) VALUES
+(1, 'test', 'test', 'test@demo.com', '$2a$10$b65a06983a40dff7b1f7euys9aDoxG9OZF40MxpT9hYq861pb/M8K', 796363200, 'male', 'email', NULL, NULL, NULL, NULL, 1544101380),
+(2, 'Armando', 'Amaya', 'armjaxd@hotmail.com', '$2a$10$7145785b291d2970bd0e9u4oh4qdcr48OtE0C3bIsIGoHrVMoVuIa', 796363200, 'male', 'fb', '10218449797824110', NULL, NULL, NULL, 1544294091),
+(3, 'bb9d201a4b', 'bbbba', 'bb9d201a4b@nicemail.pro', '$2a$10$21e040f96f4346cc08accOKnREHC6K28FolR2lGR7I3qwKCouX4CG', 946699200, 'male', 'tc', '254800000', NULL, NULL, NULL, 1544297223);
 
 --
 -- Índices para tablas volcadas
@@ -68,7 +71,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
